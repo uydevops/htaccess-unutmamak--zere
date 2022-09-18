18962fc5eb19d82eccd68d4f26d919d5e2490731
UgurcanYAS <>
2022-09-18 14:57:20 +0300

    first commit

---
+++ db.php
@@ -0,0 +1,6 @@
+<?php
+$db = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+
+
+
+
---
+++ post.php
@@ -0,0 +1,8 @@
+<?php
+
+
+if(isset($_GET["name"])){
+    $name = $_GET["name"];
+    echo $name." ".$_GET["test"];
+}
+?>
